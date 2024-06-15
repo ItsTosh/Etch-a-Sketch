720dc6e6d91baec8776a563b6b6b18cebc0ddc54
Tosh <128783853+ItsTosh@users.noreply.github.com>
2024-06-15 15:37:07 +1000

    Finshed Container
    Added necessary CSS and JavaScript for the container

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const container = document.querySelector("#container")
-let numberOfSqr = (16*16);
+let numberOfSqr = 64;
+let containerSize = 400;
+let sqrSize = containerSize / numberOfSqr
 
-for (let i = 0; i < numberOfSqr; i++) {
+for (let i = 0; i < numberOfSqr * numberOfSqr; i++) {
   const div = document.createElement("div");
-  div.textContent = 1;
-
-  div.classList.add(i)
-
+  div.textContent = "";
+  div.style.height = `${sqrSize}px`;
+  div.style.width = `${sqrSize}px`;
+  div.style.border = "1px solid darkgray"; // Add this to see the grid lines
+  
   container.append(div);
 }
+
