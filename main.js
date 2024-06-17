4c9e45a1205137ed624221110c26bbabe876f8d5
Tosh <128783853+ItsTosh@users.noreply.github.com>
2024-06-17 18:16:35 +1000

    Color grid
    Added color to grid - no need for hover, mousehold is sufficient

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 const container = document.querySelector("#container")
-let numberOfSqr = 64;
+let numberOfSqr = 16;
 let containerSize = 400;
-let sqrSize = containerSize / numberOfSqr
+let sqrSize = containerSize / numberOfSqr; 
+let mouseDown = false; 
 
-for (let i = 0; i < numberOfSqr * numberOfSqr; i++) {
-  const div = document.createElement("div");
-  div.textContent = "";
-  div.style.height = `${sqrSize}px`;
-  div.style.width = `${sqrSize}px`;
-  div.style.border = "1px solid darkgray"; // Add this to see the grid lines
-  
-  container.append(div);
+document.body.onmousedown = () => {
+  mouseDown = true;
 }
 
+document.body.onmouseup = () => {
+  mouseDown = false;
+}
+
+function initializeGrid() {
+  for (let i = 0; i < numberOfSqr * numberOfSqr; i++) {
+    // Creation of grid boxes
+    const div = document.createElement("div");
+    div.setAttribute("id", `${i}`)
+    div.textContent = "";
+    div.style.height = `${sqrSize}px`;
+    div.style.width = `${sqrSize}px`;
+    div.style.border = "1px solid darkgray"; // Added this to see the grid lines (temporary)
+
+    // Grid Color
+    div.addEventListener("mouseover", () => {
+      if (mouseDown) {
+        div.style["backgroundColor"] = "#333";
+      }
+    })
+
+    container.append(div);
+  }
+}
+
+
+
+
+initializeGrid();
